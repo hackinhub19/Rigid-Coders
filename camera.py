9136b2aa40f49d96759572f6e4906f9071667f35
RamNathan691 <ramnathan0691@gmail.com>
2019-09-13 03:20:09 +0530

    created akivy app need to convert it into the android app

---
+++ camera.py
@@ -0,0 +1,65 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+import pytesseract
+from PIL import Image
+from gtts import gTTS 
+import os 
+import cv2
+Builder.load_string('''
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+''')
+
+
+class CameraClick(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        cam = cv2.VideoCapture(0)
+
+        ret,frame=cam.read()
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        img_name="IMG_{}.png".format(timestr)
+        print("{} written!".format(img_name))
+        cv2.imwrite(img_name)
+        cam.release()
+        cv2.destroyAllWindows()
+        pytesseract.pytesseract.tesseract_cmd=r"C:/Program Files (x86)/Tesseract-OCR/tesseract"
+        img=Image.open(img_name,frame)
+        text=pytesseract.image_to_string(img)
+        print(text)
+        mytext = text
+        language = 'en'
+        myobj = gTTS(text=mytext, lang=language, slow=False) 
+        myobj.save("welcome.mp3") 
+        os.system("welcome.mp3")
+        print("Captured")
+       # frame.reshape(-1,1)
+        
+
+class TestCamera(App):
+
+    def build(self):
+        return CameraClick()
+
+
+TestCamera().run()
